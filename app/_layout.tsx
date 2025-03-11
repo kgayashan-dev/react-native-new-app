a38ad2fb588831a147c24357184381d41126a91f
gayashan-dev <mgayashan83@gmail.com>
2025-03-11 13:27:50 +0530

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,5 @@ import { Stack } from "expo-router";
 import "./globals.css";
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
